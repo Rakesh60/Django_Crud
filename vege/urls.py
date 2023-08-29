313d09a43af5e0d8aee479265fd2f4c38b7526ab
Rakesh60 <rakeshgupta5353@gmail.com>
2023-08-30 00:18:04 +0530

    student_marks

--- vege/urls.py
+++ vege/urls.py
@@ -2,14 +2,15 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-     path('vege/',views.reciepies,name='reciepe'),
-     path('delete/<rec_id>/',views.delete_reciepe,name='reciepe'),
-     path('update/<rec_id>/',views.update_reciepe,name='reciepe'),
-     path('login/',views.login_handel,name='login'),
-     path('register/',views.register_handel,name='register'),
-     path('logout/',views.logout_handel,name='logout'),
-     path('students/',views.get_students,name='get_studentss'),
-     
-     
+urlpatterns = [
+    path('vege/', views.reciepies, name='reciepe'),
+    path('delete/<rec_id>/', views.delete_reciepe, name='reciepe'),
+    path('update/<rec_id>/', views.update_reciepe, name='reciepe'),
+    path('login/', views.login_handel, name='login'),
+    path('register/', views.register_handel, name='register'),
+    path('logout/', views.logout_handel, name='logout'),
+    path('students/', views.get_students, name='get_studentss'),
+    path('see_marks/<student_id>/', views.see_marks, name='see_marks'),
+
+
 ]
--- vege/views.py
+++ vege/views.py
@@ -1,3 +1,5 @@
+
+from django.db.models import Q, Sum
 from django.shortcuts import render, redirect, HttpResponse
 from .models import *
 from django.contrib.auth.models import User
@@ -135,13 +137,23 @@ def get_students(request):
     queryset = Student.objects.all()
 
     if request.GET.get('search'):
-        search=request.GET.get('search')
-        queryset=queryset.filter(Student_name__icontains=search)
+        search = request.GET.get('search')
+        queryset = queryset.filter(
 
-
+            Q(student_id__student_id__icontains=search) | Q(
+                student_name__icontains=search) | Q(student_email__icontains=search) | Q(department__department__icontains=search)
+        )
     # Pagination
     paginator = Paginator(queryset, 15)  # Show 25 contacts per page.
     page_number = request.GET.get("page", 1)
     page_obj = paginator.get_page(page_number)
 
     return render(request, 'report/students.html', {'queryset': page_obj})
+
+
+def see_marks(request, student_id):
+    queryset = SubjectMarks.objects.filter(
+        student__student_id__student_id=student_id)
+    total_marks = queryset.aggregate(total_marks=Sum('marks'))
+
+    return render(request, 'report/see_marks.html', {'queryset': queryset, 'total_marks': total_marks})
