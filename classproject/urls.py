98b6c03b240f022ee49a0e2552ca58a6cc3fc98a
Rakesh60 <rakeshhupta5353@gmail.com>
2024-03-01 10:57:56 +0530

    button enabled

--- classproject/urls.py
+++ classproject/urls.py
@@ -29,7 +29,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/',about,name='about'),
-    path('',home,name='home'),
+    #path('',home,name='home'),
     path('', include('vege.urls')),
    
 
